620e97fc19776ce0c7f9b3c50948339e59d09e06
Dhruvik Karena <dhruvikkarena1803@gmail.com>
2022-10-21 12:14:41 +0530

    fixed warnings for WebSecurityConfigurerAdapter

--- src/main/java/com/project/registrationlogin/config/SecurityConfiguration.java
+++ src/main/java/com/project/registrationlogin/config/SecurityConfiguration.java
@@ -4,18 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.project.registrationlogin.service.UserService;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+public class SecurityConfiguration {
 
     @Autowired
 	private UserService userService;
@@ -33,13 +32,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
         return auth;
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+     
         http.authorizeRequests().antMatchers(
 				 "/registration**",
 	                "/js/**",
@@ -57,5 +52,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 		.logoutSuccessUrl("/login?logout")
 		.permitAll();
+
+        http.authenticationProvider(authenticationProvider());
+ 
+        return http.build();
     }
 }
